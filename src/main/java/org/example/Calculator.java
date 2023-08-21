49da62d530a803f9aff9683070c8359761f7445d
alexandravramenko <avramenko423@gmail.com>
2023-08-21 20:36:49 +0300

    add project

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Calculator {
+    public float add(float a, float b) {
+        return a + b;
+    }
+
+    public float subtraction(float a, float b) {
+        return a - b;
+    }
+
+    public float division(float a, float b) {
+        return a / b;
+    }
+
+    public float multiply(float a, float b) {
+        return a * b;
+    }
+}
